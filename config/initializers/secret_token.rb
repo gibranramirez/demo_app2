15c150b122ef5c84dd7a342f9f109a12dc6c12e1
gibranramirez <gibran.abarca@gmail.com>
2012-04-15 12:22:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp2::Application.config.secret_token = '99f14c0f189e58f2937335bf876fd2e49e44a892e82372bb51f8b1ba40a1d509995229de8558ce25317cd79da87b905ec0d6773f57d59d1a8ea26f1a83d316d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DemoApp2::Application.config.session_store :cookie_store, :key => '_demo_app2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DemoApp2::Application.config.session_store :active_record_store
